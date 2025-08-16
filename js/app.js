6ff87e9c98bd0383ed384ba32acfb2bbb1af5a7b
FELIPE ALVES FEZZI <86987347+felipefezzi1978@users.noreply.github.com>
2025-08-16 14:48:45 -0300

    Inclusão funcionalidade contar jogos alugados e imprimir na console

--- js/app.js
+++ js/app.js
@@ -3,13 +3,32 @@ function alterarStatus(id) {
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
 
-    if (imagem.classList.contains('dashboard__item__img--rented')) {
+    const estaAlugado = imagem.classList.contains('dashboard__item__img--rented');
+
+    if (estaAlugado) {
+        if (!confirm('Você tem certeza que deseja devolver este jogo?')) {
+            return; // Se não confirmar, sai da função
+        } 
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';        
+        botao.textContent = 'Alugar';
     } else {
-        imagem.classList.add('dashboard__item__img--rented');
-        botao.textContent = 'Devolver';
-        botao.classList.add('dashboard__item__button--return');    
+    
+    imagem.classList.add('dashboard__item__img--rented');
+    botao.textContent = 'Devolver';
+    botao.classList.add('dashboard__item__button--return');
+    botao.textContent = 'Devolver';
 }
+contarJogosAlugados();
+}
+
+function contarJogosAlugados() {
+  // Seleciona todos os elementos que estão com a classe de alugado
+  const jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented');
+  
+  // Conta quantos foram encontrados
+  const total = jogosAlugados.length;
+
+  // Mostra no console
+  console.log(`Total de jogos alugados: ${total}`);
 }
